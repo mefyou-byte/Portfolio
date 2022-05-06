f21ab2572ffcebd338cfd1622a005a9e385feea9
Faltas <mfaltas@dccsintra.net>
2022-05-06 19:54:23 +0200

    changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,14 +10,14 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Matiou Faltas </span>
             from <span className="purple">Graz, Austria.</span>
-            <br />I'm a Software Engineer from Graz, Austria. I graduated 2019 from the Higher
-            Technical College Graz-Gösting of Electronics & Technical Computer Science and got my Diploma. Creating, maintain, audit and improve systems in particular need is the role i have. 
+            <br />I'm a Software Engineer. I graduated <span className="purple">2019</span> from the <span className="purple">Higher
+            Technical College Graz-Gösting </span> of Electronics & Technical Computer Science and got my Diploma. Creating, maintain, audit and improve systems in particular need is the role i have. 
             Such as desigining and writing some cool Code for dynamic applications, is my passion.
             <br />
             <br />
             After my 5-year journey at college, I have built a solid foundation 
             of computer science. In order to continue my education in computer
-            science and also to acquire economic skills, I decided in 2020 to start a bachelor's degree in Business Informatics at FH CAMPUS 02. 
+            science and also to acquire economic skills, I decided in <span className="purple">2020</span> to start a <span className="purple">bachelor's degree in Business Informatics</span> at FH CAMPUS 02. 
             I work as a software engineer for DCCS IT Business Solution.
           </p>
           <ul>
